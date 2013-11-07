9d8c4539413402f36c8cc962969da7f038fa0561
Molo <arnaud@onesime.fr>
2013-11-07 18:02:41 +0100

    [templating] oeuvres

--- sites/all/themes/bigdaddy/template.php
+++ sites/all/themes/bigdaddy/template.php
@@ -212,4 +212,13 @@ function bigdaddy_form_your_form_id_alter(&$form, &$form_state) {
 
 }
 
+function bigdaddy_menu_link(array $variables){
+  $element = $variables['element'];
+  $sub_menu = '';
 
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
